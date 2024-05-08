3ffc24734c25469e4671cacee010c339b485606f
ERICK10_Y <144359200+huamaniERICK@users.noreply.github.com>
2024-05-08 08:16:28 -0500

    mi archivo lecture.py

---
+++ lecture.py
@@ -0,0 +1,10 @@
+# aser un programa que verifique si un numero ingresado por el usuario es positivo o negativo.
+numero=int(input("ingrese un numero: "))
+if numero > 0:
+    print("el numero ingresado es positivo")
+else:
+    print("el numero ingresado es cero")
+
+#aser un programa que imprima del 1 al 5
+for numero in range(1 , 6):
+    print(numero)
